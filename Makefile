9d9c6e1be7d565dfb65f46e3cccd7004f9f5f645
bys-exe <batchuyugasai@gmail.com>
2026-06-22 14:52:33 +0000

    parser

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+all: prompt parser
+
 prompt: prompt.c
-	$(CC) prompt.c -o prompt -Wall -std=c99 -g -ledit
+	$(CC) prompt.c -o prompt -Wall -std=c99 -g -ledit -lm
+
+parser: parser.c
+	$(CC) parser.c -o parser lib/mpc.c -Wall -std=c99 -g -lm
+
--- prompt.c
+++ prompt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "lib/mpc.c"
 
 #ifdef _WIN32 // if we are compiling from windows run this
 #include <string.h>
@@ -21,14 +22,40 @@ void add_history(char *unused) {} // fake add_history func
 
 int main(int argc, char **argv)
 {
+    mpc_parser_t *Number = mpc_new("number");
+    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Expr = mpc_new("expr");
+    mpc_parser_t *Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+              " \
+                number : /-?[0-9]+(.[0-9]+)?/; \
+                operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\"; \
+                expr : <number> | '(' <operator> <expr>+ ')'; \
+                lispy : /^/ <expr>* <operator> <expr>* /$/; \
+              ",
+              Number, Operator, Expr, Lispy);
+
     puts("Lispy version 0.0.0.0.1"); // version
     puts("Press Ctrl+c to Exit\n");  // guide
+
     while (1)
     {
         char *input = readline("lispy> "); // output prompt and take input from user
         add_history(input);                // add input to history
-        printf("No u are %s\n", input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r))
+        {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        }
+        else
+        {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
         free(input);
     }
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
